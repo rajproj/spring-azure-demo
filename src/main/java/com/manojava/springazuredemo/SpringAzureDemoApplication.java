c4f4c8bda71e23d4cea3fb9feab59ee7f0fcd847
rajproj <crhkprj@gmail.com>
2024-05-05 02:00:59 +0530

    Initial commit

---
+++ src/main/java/com/manojava/springazuredemo/SpringAzureDemoApplication.java
@@ -0,0 +1,20 @@
+package com.manojava.springazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+	@GetMapping("/message")
+	public String message(){
+		return "Welcome to app spring azure demo .....";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
