2407cf828ffa9a520413570305fc7b59466aa7dd
rajproj <crhkprj@gmail.com>
2024-06-13 02:58:15 +0530

    Updating the message

--- src/main/java/com/manojava/springazuredemo/SpringAzureDemoApplication.java
+++ src/main/java/com/manojava/springazuredemo/SpringAzureDemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringAzureDemoApplication {
 	@GetMapping("/message")
 	public String message(){
-		return "Welcome to app spring azure demo .....";
+		return "Hi! Welcome !! This application is build with springboot3 gradle githubactions and azure webapp";
 	}
 
 	public static void main(String[] args) {
